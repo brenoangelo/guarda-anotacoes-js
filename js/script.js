7ecb05eaab1087c8b69a1b8b754f4d9b6507faca
brenoangelo <brenoangeloal@gmail.com>
2021-05-14 16:46:48 -0300

    Finalizado a persistencia de dados

--- js/script.js
+++ js/script.js
@@ -1,17 +1,85 @@
-const anotacoes = [
-    {titulo: 'Minha senha', conteudo: 'loremipsum123'},
-    {titulo: 'Fazer prova', conteudo: 'A prova será dia 23/05'},
-    {titulo: 'Livros para comprar', conteudo: 'O hobbit, Senhor dos aneis, O último desejo'},
-    {titulo: 'Estudar front end', conteudo: 'Aprender ReactJs, Angular, VueJs'}
-]
+
+var anotacoes= localStorage.getItem('anotacoes')
+anotacoes = JSON.parse(anotacoes)
+
+// Variáveis
+let btn_anotar = document.querySelector('#btn-anotar')
+let titulo = document.querySelector('#titulo')
+let conteudo = document.querySelector('#conteudo')
 
 
 exibir()
+limparCampos()
+
+btn_anotar.addEventListener('click', adicionar)
+
+function adicionar(){
+    
+    if(titulo.value == "" || titulo.value == null){
+        alert("Está faltando o titulo")
+        return false
+    }
+
+    if(conteudo.value == "" || conteudo.value == null){
+        alert("Está faltando a descrição")
+        return false
+    }
+
+    if(titulo.value.length > 35){
+        alert('O título está muito longo')
+        return false
+    }
+
+    if(conteudo.value.length > 244){
+        alert('O conteúdo está gigantesco! :0')
+        return false
+    }
+
+    salvar(titulo.value, conteudo.value)
+    fecharPopUp()
+    limparCampos()
+    exibir()
+    
+}
+
+function salvar(titulo, conteudo){
+    const nota = {
+        titulo: titulo,
+        conteudo: conteudo
+    }
+    
+    
+    anotacoes.push(nota)
+    localStorage.setItem("anotacoes", JSON.stringify(anotacoes))
+    
+    return true
+}
+
+function remover(){
+    let btn_remover = document.querySelectorAll('.remover-nota')
+
+    btn_remover.forEach((value, index)=>{
+        btn_remover[index].addEventListener('click',()=>{
+            alert('legal')
+            anotacoes.splice(index,1)
+            localStorage.setItem('anotacoes', JSON.stringify(anotacoes))
+            exibir()
+        })
+
+        
+    })
+    
+}
 
 function exibir(){
     let blocosContainer = document.querySelector('.blocos')
+    blocosContainer.innerHTML = ""
+
     anotacoes.forEach((value, index)=>{
+
+        
         blocosContainer.innerHTML += `<div class="bloco-single">
+                                        <span class="remover-nota"><i class="fas fa-recycle"></i></span>
                                         <h2>${value.titulo}</h2>
                                         <p>${value.conteudo}</p>
                                     </div>`
@@ -19,6 +87,15 @@ function exibir(){
         bloco_single[index].style.backgroundColor = gerar_cor()
         
     })
+
+    
+
+    remover()
+}
+
+function limparCampos(){
+    titulo.value = ""
+    conteudo.value = ""
 }
 
 
@@ -54,6 +131,3 @@ function abrirPopUp(){
 function fecharPopUp(){
     popup.style.display = "none"
 }
-
-
-
